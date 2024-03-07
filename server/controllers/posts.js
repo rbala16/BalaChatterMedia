248072388202b8c195f7d9b90a15424d76022ae9
Rajni Bala <balatamna16@gmail.com>
2024-03-06 20:25:00 -0500

    controllers post updated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -16,9 +16,60 @@ import Post from "../models/Post.js";
         comments:[]
      })
      await newPost.save();
-     
+
+     const post = await Post.find();
+
+     res.status(201).json(post);    //return to frontend 
     }
     catch(err){
         res.status(409).json({message:err.message})
     }
  }
+
+//  read
+export const getFeedPosts = async(req,res)=>{
+   try{
+      const post = await Post.find();
+       res.status(200).json(post);
+   }
+   catch(err){
+      res.status(404).json({message:err.message})
+  }
+}
+
+export const getUserPosts = async(req,res)=>{
+   try{
+      const {userId} = req.params;
+      const post = await Post.find({userId});
+       res.status(200).json(post);
+   }
+   catch(err){
+      res.status(404).json({message:err.message})
+  }
+}
+
+// update
+export default likePost = async(req,res)=>{
+   try{
+    const {id} = req.params; //query string
+    const {userId} = req.body;
+    const post = await Post.findbyId(id);
+    const isLiked = post.likes.get(userId);
+
+    if(isLiked){
+      post.likes.delete(userId);
+    }
+    else{
+      post.likes.set(userId,true);
+    }
+    const updatedPost = await Post.findbyIdAndUpdate(   //update frontend
+      id,
+      {likes:post.likes},
+      {new:true}
+    );
+       res.status(200).json(updatedPost);
+   }
+   catch(err){
+      res.status(404).json({message:err.message})
+  }
+}
