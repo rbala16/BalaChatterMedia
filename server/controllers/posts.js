a91ca22f0ba48c4dec2cc18da1f02dbb43e22971
Rajni Bala <balatamna16@gmail.com>
2024-03-06 19:58:55 -0500

    create post controller updated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,1 +1,24 @@
 import Post from "../models/Post.js";
+// Create
+ export const CreatePost = async(req,res)=>{
+    try{
+     const {userId,description,picturePath} = req.body;
+     const user = await User.findbyId(userId);
+     const newPost = new Post({
+        userId,
+        firstName:user.firstName,
+        lastName:user.lastName,
+        location:user.location,
+        description,
+        userPicturePath:user.picturePath,
+        picturePath,
+        likes:{},
+        comments:[]
+     })
+     await newPost.save();
+     
+    }
+    catch(err){
+        res.status(409).json({message:err.message})
+    }
+ }
