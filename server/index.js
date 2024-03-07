f45c7552ccd7387b3ff4580229850d892e494f48
Rajni Bala <balatamna16@gmail.com>
2024-03-06 21:51:02 -0500

    index.js file updated

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import {createPost} from "./controllers/posts.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import {users,posts} from "./data/index.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -56,5 +59,8 @@ const PORT = process.env.PORT || 6001;
 mongoose
 .connect(process.env.MONGO_URL).then(()=>{
     app.listen(PORT,() => console.log(`Server Port: ${PORT}`));
+    // // add data one time(only when i need)
+    // User.insertMany(users);
+    // Post.insertMany(posts);
     })
     .catch((error)=> console.log(`${error} did not connect`));
