5f431d18f8ada7b5ea430738196b7d62763c4852
Rajni Bala <balatamna16@gmail.com>
2024-03-07 16:14:23 -0500

    set up routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-
 import './App.css';
+import { BrowserRouter,Navigate,Routes,Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage/homePage';
+import LoginPage from 'scenes/loginPage/loginPage';
+import profilePage from 'scenes/profilePage/profilePage';
 
 function App() {
+
   return (
     <div className="App">
-      
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<loginPage/>} />
+        <Route path='/home' element={<HomePage/>} />
+        <Route path='/profile/:userId' element={<profilePage/>} />    
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
